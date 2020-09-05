48fefc70ec44d63eab21920ef8fd8ede38849579
cloudXA <zming794@gmail.com>
2020-09-05 11:09:03 +0800

    保存react component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
+import querystring from 'query-string';
+import io from 'socket.io-client';
 import './Chat.css'
 
-export class Chat extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Chat</h1>
-      </div>
-    )
-  }
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  
+
+  // 相当于componentDidMount 和 componentDidUpdate 
+  useEffect (() => {
+    // 解析url为： ?name=1&room=1 为 {name: 1, room: 1}
+    const {name, room} = querystring.parse(location.search);
+
+    socket = io();
+
+    setName(name);
+    setRoom(room);
+    
+  });
+
+  return (
+    <div>
+      <p>you clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        click me 
+      </button>
+    </div>
+  )
 }
 
 export default Chat
--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Join.css'
+import './Join.scss'
 
 
 
